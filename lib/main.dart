8bd2e08e814929bcc90f76f356d368fea25bf1c2
agamairi <agam.airi@gmail.com>
2024-11-21 22:43:36 -0500

    initalizing db and updating main function to run the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_bloc/data/models/isar_todo.dart';
+import 'package:todo_bloc/data/repository/isar_todo_repo.dart';
+import 'package:todo_bloc/domain/repository/todo_repo.dart';
+import 'package:todo_bloc/presentation/todo_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationDocumentsDirectory();
+
+  final isar = await Isar.open([TodoIsarSchema], directory: dir.path);
+
+  final isarRepo = IsarTodoRepo(isar);
+
+  runApp(MyApp(
+    todoRepo: isarRepo,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final TodoRepo todoRepo;
+
+  const MyApp({super.key, required this.todoRepo});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: TodoPage(
+        todoRepo: todoRepo,
+      ),
     );
   }
 }
